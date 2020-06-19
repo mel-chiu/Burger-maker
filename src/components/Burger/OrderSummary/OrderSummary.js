811d9d91e144d6af50ea7144866dfcfd8ffd47a0
mel-chiu <melinda.hsinying.chiu@gmail.com>
2020-06-19 23:24:46 +0200

    Finish the basic app design

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,26 +2,29 @@ import React from 'react';
 import Auxi from '../../../HOC/Auxi';
 import Button from '../../UI/ButtonStyle/ButtonStyle'
 
-const OrderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients).map((ingredientsKey) => {
+class OrderSummary extends React.Component {
+
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients).map((ingredientsKey) => {
+            return (
+                <li key={ingredientsKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{ingredientsKey}</span> : {this.props.ingredients[ingredientsKey]}
+                </li>);
+        });
         return (
-        <li key={ingredientsKey}>
-            <span style={{ textTransform: 'capitalize' }}>{ingredientsKey}</span> : {props.ingredients[ingredientsKey]}
-        </li>);
-    });
-    return (
-        <Auxi>
-            <h3>Your Order</h3>
-            <p>Your Burger includes:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p>Total price: &euro;{props.price.toFixed(2)}</p>
-            <p>Check out?</p>
-            <Button btnType = {"Success"} clicked={props.continuePurchase}>Yes, continue</Button>
-            <Button btnType = {"Danger"} clicked={props.canclePurchase}>Later</Button>
-        </Auxi>
-    )
+            <Auxi>
+                <h3>Your Order</h3>
+                <p>Your Burger includes:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p>Total price: &euro;{this.props.price.toFixed(2)}</p>
+                <p>Check out?</p>
+                <Button btnType={"Success"} clicked={this.props.continuePurchase}>Yes, continue</Button>
+                <Button btnType={"Danger"} clicked={this.props.canclePurchase}>Later</Button>
+            </Auxi>
+        )
+    }
 }
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,20 +3,23 @@ import classes from './Modal.module.css';
 import Auxi from '../../../HOC/Auxi';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-    return (
-        <Auxi>
-            <Backdrop show = {props.show} goBack = {props.modalClosed}/>
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-        </Auxi>
-    );
+class Modal extends React.Component {
+
+    render() {
+        return (
+            <Auxi>
+                <Backdrop show={this.props.show} goBack={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Auxi>
+        );
+    }
 }
 
 export default Modal;
